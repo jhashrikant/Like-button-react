4a5236717ec9422fdf9f80c0abb1b4ebb05155af
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2024-07-08 15:33:32 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Heart ,Loader } from 'lucide-react';
 
 function App() {
+
+  const [liked, setLiked] = useState(false)
+  const [Loading, setLoading] = useState(false)
+  const [error, seterror] = useState("")
+
+
+  const handleLikeUnlike = async () => {
+    setLoading(true)
+    seterror('')
+    try {
+      const response = await fetch("https://www.greatfrontend.com/api/questions/like-button", {
+        method: "POST",
+        body: JSON.stringify({ action: liked ? "unlike" : "like" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await response.json();
+      console.log('ok', json);
+      if (json.message === 'Success!') {
+        setLiked(!liked)
+        setLoading(false)
+      }
+      else {
+        setLoading(false)
+        seterror(json.message)
+        return
+      }
+    } catch (error) {
+      console.error(error.message);
+      setLoading(false)
+      seterror(error.message)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <button className='likeBtn' onClick={handleLikeUnlike} style={{ backgroundColor: liked ? 'red' : '' }}>
+        {Loading ? <Loader /> : <Heart className='heartIcon' />}
+        {liked ? 'Liked' : 'Like'}
+      </button>
+      {error && <div className='error'>{error}</div>}
     </div>
   );
 }
